f288d724570715bee0189839302f394be0cfe897
giannurwana19 <giannurwana19@gmail.com>
2021-04-16 11:23:10 +0700

    instalasi package react-bootstrap, axios, sweetalert2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import NavbarComp from './components/NavbarComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavbarComp />
+      <Container>
+        <h1>Halo ini navbar comp</h1>
+      </Container>
+    </>
   );
 }
 
