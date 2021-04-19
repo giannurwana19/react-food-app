c00111974eb5706c9227d26f4fc57f56d816912f
giannurwana19 <giannurwana19@gmail.com>
2021-04-19 16:42:04 +0700

    delete component CartBlank

--- src/components/ResultComp.js
+++ src/components/ResultComp.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Badge, Col, ListGroup, Row, Card } from 'react-bootstrap';
 import { formatNumber } from '../utils/utils';
-import CartBlank from './CartBlank';
 import Payment from './Payment';
 
 class ResultComp extends React.Component {
@@ -19,35 +18,31 @@ class ResultComp extends React.Component {
         <hr />
         <Card className="overflow-auto result">
           <ListGroup>
-            {this.props.carts.length > 0 ? (
-              this.props.carts.map(cart => (
-                <ListGroup.Item
-                  style={{ cursor: 'pointer' }}
-                  key={cart.id}
-                  onClick={() => this.pushToCartDetail(cart)}>
-                  <Row>
-                    <Col xs="2">
-                      <h5>
-                        <Badge pill variant="success">
-                          {cart.total}
-                        </Badge>
-                      </h5>
-                    </Col>
-                    <Col>
-                      <h6>{cart.product.name}</h6>
-                      <p>Rp. {formatNumber(cart.product.price)}</p>
-                    </Col>
-                    <Col>
-                      <strong className="float-right">
-                        Rp. {formatNumber(cart.totalPrice)}
-                      </strong>
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-              ))
-            ) : (
-              <CartBlank />
-            )}
+            {this.props.carts.map(cart => (
+              <ListGroup.Item
+                style={{ cursor: 'pointer' }}
+                key={cart.id}
+                onClick={() => this.pushToCartDetail(cart)}>
+                <Row>
+                  <Col xs="2">
+                    <h5>
+                      <Badge pill variant="success">
+                        {cart.total}
+                      </Badge>
+                    </h5>
+                  </Col>
+                  <Col>
+                    <h6>{cart.product.name}</h6>
+                    <p>Rp. {formatNumber(cart.product.price)}</p>
+                  </Col>
+                  <Col>
+                    <strong className="float-right">
+                      Rp. {formatNumber(cart.totalPrice)}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
           </ListGroup>
         </Card>
         <Payment carts={this.props.carts} {...this.props} />
